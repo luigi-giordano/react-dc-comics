5f750e7d1d4684d0ca66464586569bd8c2348f87
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-15 11:56:14 +0100

    fix css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,54 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="footer-links">
-          <div>
-            <h4>DC COMICS</h4>
-            <ul>
-              <li>Characters</li>
-              <li>Comics</li>
-              <li>Movies</li>
-              <li>TV</li>
-              <li>Games</li>
-              <li>Videos</li>
-              <li>News</li>
-            </ul>
-          </div>
-          <div>
-            <h4>SHOP</h4>
-            <ul>
-              <li>Shop DC</li>
-              <li>Shop DC Collectibles</li>
-            </ul>
-          </div>
-          <div>
-            <h4>DC</h4>
-            <ul>
-              <li>Terms Of Use</li>
-              <li>Privacy Policy</li>
-              <li>Ad Choices</li>
-              <li>Advertising</li>
-              <li>Jobs</li>
-              <li>Subscriptions</li>
-              <li>Talent Workshop</li>
-              <li>CPSC Certificates</li>
-              <li>Ratings</li>
-              <li>Shop Help</li>
-              <li>Contact Us</li>
-            </ul>
-          </div>
-          <div>
-            <h4>SITES</h4>
-            <ul>
-              <li>DC</li>
-              <li>MAD Magazine</li>
-              <li>DC Kids</li>
-              <li>DC Universe</li>
-              <li>DC Power Visa</li>
-            </ul>
-          </div>
+      <div className="footer-links">
+        <div>
+          <h4>DC COMICS</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
         </div>
-        <button className="sign-up">SIGN-UP NOW!</button>
+        <div>
+          <h4>SHOP</h4>
+          <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+        <div>
+          <h4>DC</h4>
+          <ul>
+            <li>Terms Of Use</li>
+            <li>Privacy Policy</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshop</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div>
+          <h4>SITES</h4>
+          <ul>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
+        </div>
       </div>
+      <button className="sign-up">SIGN-UP NOW!</button>
       <div className="footer-social">
         <p>FOLLOW US</p>
         <div className="social-icons">
