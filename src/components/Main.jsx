4c6f5b1f902662622d1c1380520ccd305b480e65
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-15 18:32:37 +0100

    fix img in main + fix css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,11 +5,11 @@ const Main = () => {
         <h2>--&gt; Content goes here &lt;--</h2>
       </div>
       <div className="main-container-boxes">
-        <div className="main-box box1"><img src="" alt="" /><p>DIGITAL COMICS</p></div>
-        <div className="main-box box2"><img src="" alt="" /><p>DC MERCHANDISE</p></div>
-        <div className="main-box box3"><img src="" alt="" /><p>SUBSCRIPTION</p></div>
-        <div className="main-box box4"><img src="" alt="" /><p>COMIC SHOP LOCATOR</p></div>
-        <div className="main-box box5"><img src="" alt="" /><p>DC POWER VISA</p></div>
+        <div className="main-box box1"><img src="/buy-comics-digital-comics.png" alt="" /><p>DIGITAL COMICS</p></div>
+        <div className="main-box box2"><img src="/buy-comics-merchandise.png" alt="" /><p>DC MERCHANDISE</p></div>
+        <div className="main-box box3"><img src="/buy-comics-shop-locator.png" alt="" /><p>SUBSCRIPTION</p></div>
+        <div className="main-box box4"><img src="/buy-comics-subscriptions.png" alt="" /><p>COMIC SHOP LOCATOR</p></div>
+        <div className="main-box box5"><img src="/buy-dc-power-visa.svg" alt="" /><p>DC POWER VISA</p></div>
       </div>
     </main>
   );
